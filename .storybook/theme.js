abb6f1cc80374ed6a9ffb7c14a4e688395951277
ParkSiHyung <86710561+ParkSiHyung@users.noreply.github.com>
2021-07-14 12:29:51 +0900

    storybook 추가

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+import yeoshin_logo from "../public/images/images/yeoshin_logo.png";
+
+export default create({
+	base: "light",
+	brandTitle: "여신티켓 Storybook",
+	brandUrl: "https://yeoshin.co.kr/",
+	brandImage: yeoshin_logo,
+});
